898b8864f1ae772cc3a4de189583076ce0686996
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-04-13 20:19:28 -0500

    feat: reproduce issue with not found and next link

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className={'flex flex-col flex-1 gap-8 items-center justify-center'}>
-      <h1 className={'text-5xl'}>Hello from Home</h1>
+      <h1 className={'text-5xl'}>Page not found</h1>
     </div>
   );
 }
